e1f88ddd8b50229e491b371cab71489c9629c6cd
Niall <niallquinn330@hotmail.com>
2025-06-07 22:32:03 +0100

    fixed bug - trying to deploy publicly

--- app.py
+++ app.py
@@ -242,4 +242,7 @@ def delete_post(post_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
